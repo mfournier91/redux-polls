2137364ab25c0c2ffa80d8277f1a1ce10efaeb59
Marc Fournier <marc.fournier91@gmail.com>
2018-07-08 14:20:20 -0400

    Added actions

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import {receivePolls} from "./polls";
+import {receiveUsers} from './users'
+import {setAuthedUser} from "./authedUser";
+
+const AUTHED_ID = 'mfournier'
+
+
+export function handleInitialData () {
+    return (dispatch) => {
+        getInitialData().then(({users, polls}) => {
+            dispatch(receiveUsers(users))
+            dispatch(receivePolls(polls))
+            dispatch(setAuthedUser(AUTHED_ID))
+        })
+    }
+}
